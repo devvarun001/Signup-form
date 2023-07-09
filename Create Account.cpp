d8fb3480301a4f1072b97a68a09da907e7c9d8ce
Varun <138992240+devvarun001@users.noreply.github.com>
2023-07-09 10:21:09 +0530

    Add files via upload

---
+++ Create Account.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	char name[12];
+	int password;
+	printf("Enter Your Name =");
+	scanf("%s", &name);
+	printf("Create a Password = ");
+	scanf("%i", &password);
+	printf("Account Created Successfullly \n");
+	printf("Code By : Dev.Varun \n");
+	
+}
